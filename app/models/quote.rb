e234cdb44977c9549da6191374e24525c04050db
Theo <theo-ng@users.noreply.github.com>
2015-11-19 10:10:20 -0800

    display random quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,4 +3,7 @@ class Quote < ActiveRecord::Base
   validates :content, presence: true, uniqueness: true
   validates :author, presence: true
   
+  def self.find_random
+    find(rand(1..count))
+  end
 end
