84232573acffbda61b5f3b48a70cde5004f8e7b9
Theo <theo-ng@users.noreply.github.com>
2015-11-18 16:35:22 -0800

    random category search on scraper

--- app/models/PexelScraper.rb
+++ app/models/PexelScraper.rb
@@ -1,18 +1,34 @@
 class PexelScraper
 
-  PATH = "https://www.pexels.com/popular-photos/#content"
+  path = "https://www.pexels.com/search/"
+  query = ["HD%20wallpaper/", "animals/", "nature/", "black-and-white/"]
+  PATH = path << query[rand(0..3)]
+
+  # PATH = "https://www.pexels.com/popular-photos/#content"
   class << self
     def get_random_pexel
+      start = Time.now
       pictures = Nokogiri::HTML(open(PATH)).xpath("//img/@src")
       index = rand(0..pictures.size-1)
       img = pictures[index].value.sub(/-medium/, '')
       too_small?(img) ? get_random_pexel : img
+      stop = Time.now
+      puts "#{stop-start}"
     end
 
     def too_small?(url)
       img = Magick::Image::read(url)[0]
-      puts "This image is #{img.columns}x#{img.rows} pixels"
+      # puts "This image is #{img.columns}x#{img.rows} pixels"
       img.columns < 1920 || img.rows < 1080
+    end
+
+    def get_all_pexels
+      pictures = []
+      Nokogiri::HTML(open(PATH)).xpath("//img/@src").each do |src|
+        img = src.value.sub(/-medium/, '')
+        pictures << img unless too_small?(img)
+      end
+      # puts "#{pictures.size} #{stop-start}"
     end
   end
 end
